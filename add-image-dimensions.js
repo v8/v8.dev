d9966b0ab62da366b4b6359f536ba6339e531fdc
Mathias Bynens <mathias@qiwi.be>
2019-08-26 14:04:17 +0200

    Update script

--- add-image-dimensions.js
+++ add-image-dimensions.js
@@ -5,21 +5,20 @@ const imageSize = require('image-size');
 
 const files = glob.sync('src/**/*.{html,md}');
 for (const file of files) {
-  if (file.includes('src/logo.md')) continue;
   const contents = fs.readFileSync(file, 'utf8').toString();
   let updatedContents = contents;
   const results = contents.matchAll(/^\s*<img.*$/gm);
   if (!results) continue;
   for (const result of results) {
     const oldLine = result[0];
-    if (!oldLine.includes('intrinsicsize')) continue;
+    if (oldLine.includes(' width="')) continue;
     const fileName = 'src/' + oldLine.match(/src="\/([^"]+)"/)[1];
     let { width, height } = imageSize(fileName);
     if (fileName.includes('@2x')) {
       width /= 2;
       height /= 2;
     }
-    const updatedLine = oldLine.replace(/ intrinsicsize="\d+x\d+"/, ` width="${width}" height="${height}"`);
+    const updatedLine = oldLine.replace(' alt="', ` width="${width}" height="${height}" alt="`);
     console.log(oldLine);
     console.log('>>');
     console.log(updatedLine);
