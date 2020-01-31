79b96743ff005b695864bfe8ec10b27cdd2a1326
tomayac <steiner.thomas@gmail.com>
2020-01-31 23:10:43 +0100

    Address review feedback

--- add-image-and-video-dimensions.js
+++ add-image-and-video-dimensions.js
@@ -7,39 +7,41 @@ const videoSize = require('get-video-dimensions');
 (async () => {
   const files = glob.sync('src/**/*.{html,md}');
   for (const file of files) {
-    const contents = fs.readFileSync(file, 'utf8').toString();
+    const contents = await fs.readFile(file, 'utf8');
     let updatedContents = contents;
     const images = contents.matchAll(/^\s*<img.*$/gm);
-    if (!images) continue;
-    for (const result of images) {
-      const oldLine = result[0];
-      if (oldLine.includes(' width="')) continue;
-      const fileName = 'src/' + oldLine.match(/src="\/([^"]+)"/)[1];
-      const { width, height } = imageSize(fileName);
-      const updatedLine = oldLine.replace(' alt="', ` width="${width}" height="${height}" alt="`);
-      console.log(oldLine);
-      console.log('>>');
-      console.log(updatedLine);
-      console.log('------------');
-      updatedContents = updatedContents.replace(oldLine, updatedLine);
+    if (images) {
+      for (const result of images) {
+        const oldLine = result[0];
+        if (oldLine.includes(' width="')) continue;
+        const fileName = 'src/' + oldLine.match(/src="\/([^"]+)"/)[1];
+        const { width, height } = imageSize(fileName);
+        const updatedLine = oldLine.replace(' alt="', ` width="${width}" height="${height}" alt="`);
+        console.log(oldLine);
+        console.log('>>');
+        console.log(updatedLine);
+        console.log('------------');
+        updatedContents = updatedContents.replace(oldLine, updatedLine);
+      }
     }
 
     // TODO: This needs updating once we use `<source>`.
     const videos = contents.matchAll(/^\s*<video.*$/gm);
-    if (!videos) continue;
-    for await (const result of videos) {
-      const oldLine = result[0];
-      if (oldLine.includes(' width="')) continue;
-      const fileName = 'src/' + oldLine.match(/src="\/([^"]+)"/)[1];
-      const { width, height } = await videoSize(fileName);
-      const updatedLine = oldLine.replace(' src="', ` width="${width}" height="${height}" src="`);
-      console.log(oldLine);
-      console.log('>>');
-      console.log(updatedLine);
-      console.log('------------');
-      updatedContents = updatedContents.replace(oldLine, updatedLine);
+    if (videos) {
+      for (const result of videos) {
+        const oldLine = result[0];
+        if (oldLine.includes(' width="')) continue;
+        const fileName = 'src/' + oldLine.match(/src="\/([^"]+)"/)[1];
+        const { width, height } = await videoSize(fileName);
+        const updatedLine = oldLine.replace(' src="', ` width="${width}" height="${height}" src="`);
+        console.log(oldLine);
+        console.log('>>');
+        console.log(updatedLine);
+        console.log('------------');
+        updatedContents = updatedContents.replace(oldLine, updatedLine);
+      }
     }
 
-    fs.writeFileSync(file, updatedContents);
+    await fs.writeFile(file, updatedContents);
   }
 })();
